ae0f89a6855bc2920fc8b3d5ca61beddceaf2a6b
Crypted Nanomet <sami.mhatre756@gmail.com>
2024-07-01 23:10:19 +0530

    bluejay: add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+# Vendor
+git clone https://gitlab.com/CYB3R0ID694/proprietary_vendor_google_bluejay.git -b 14-blaze vendor/google/bluejay --depth=1
+
+# Kernel
+git clone https://android.googlesource.com/device/google/bluejay-kernel.git device/google/bluejay-kernel --depth=1
+
+# gs101
+git clone https://github.com/ProjectBlaze-Devices/device_google_gs101 -b 14 device/google/gs101
+
+# Gcam
+git clone https://gitlab.com/CYB3R0ID694/vendor_gcam -b CYB3R0ID694-main-patch-47042 vendor/gcam --depth=1
