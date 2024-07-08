bf79088f253e66058e44ad060014cdc998aa9fe1
Crypted Nanomet <sami.mhatre756@gmail.com>
2024-07-09 00:41:52 +0530

    bluejay-vendorsetup: change branch for vendor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-git clone https://gitlab.com/CYB3R0ID694/proprietary_vendor_google_bluejay.git -b 14-blaze vendor/google/bluejay
+git clone https://gitlab.com/CYB3R0ID694/proprietary_vendor_google_bluejay.git -b A14-blaze vendor/google/bluejay
 git clone https://android.googlesource.com/device/google/bluejay-kernel.git --depth=1 device/google/bluejay-kernel
 git clone https://github.com/ProjectBlaze-Devices/device_google_gs101.git -b 14 device/google/gs101
 git clone https://gitlab.com/CYB3R0ID694/vendor_gcam.git -b CYB3R0ID694-main-patch-47042 vendor/gcam
